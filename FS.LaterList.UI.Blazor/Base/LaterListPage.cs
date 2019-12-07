b7992e35acb7d4c089bca8dd39b4835edd8b7a68
Florian Schick <florian.schick@f-schick.de>
2019-12-07 13:51:09 +0100

    Initial check-in.

---
+++ FS.LaterList.UI.Blazor/Base/LaterListPage.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+
+namespace FS.LaterList.UI.Blazor.Base
+{
+    public abstract class LaterListPage : ComponentBase
+    {
+        [Inject] protected HttpClient HttpClient { get; set; }
+    }
+}
---
+++ FS.LaterList.UI.Blazor/Pages/Index.razor.cs
@@ -0,0 +1,21 @@
+﻿using FS.LaterList.Common.Models;
+using FS.LaterList.Common.Routing;
+using FS.LaterList.UI.Blazor.Base;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FS.LaterList.UI.Blazor.Pages
+{
+    public class IndexController : LaterListPage
+    {
+        protected IEnumerable<TodoList> TodoLists = Enumerable.Empty<TodoList>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            TodoLists = await HttpClient.GetJsonAsync<IEnumerable<TodoList>>(Routes.LaterList.GetTodoLists);
+        }
+    }
+}
