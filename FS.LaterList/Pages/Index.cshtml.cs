641f046d92b1bcaa15336bdf3430993b960c89f3
Florian Schick <florian.schick@schick-software.de>
2019-12-19 17:22:33 +0100

    Bootstrap updated to 4.4.1. Deliver non-minified css/js files while development (for server rendered variant).

---
+++ FS.LaterList/Pages/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FS.LaterList.Pages
+{
+    public class IndexController : PageModel
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public bool IsDevelopment => false;//_webHostEnvironment.IsDevelopment();
+
+        public IndexController(IWebHostEnvironment webHostEnvironment)
+            => _webHostEnvironment = webHostEnvironment;
+    }
+}
