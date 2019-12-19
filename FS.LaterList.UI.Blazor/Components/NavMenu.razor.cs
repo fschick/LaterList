307aaaf3419f0bb7b9912eef0ce7d7e5b3ba3240
Florian Schick <florian.schick@schick-software.de>
2019-12-19 18:00:43 +0100

    Show current bootstrap breakpoint in debug builds.

---
+++ FS.LaterList.UI.Blazor/Components/NavMenu.razor.cs
@@ -0,0 +1,14 @@
+﻿using FS.LaterList.UI.Blazor.Base;
+
+namespace FS.LaterList.UI.Blazor.Components
+{
+    public class NavMenuController : LaterListComponent
+    {
+        protected bool IsDevelopment =
+#if DEBUG
+            true;
+#else
+            false;
+#endif
+    }
+}
