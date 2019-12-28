a7de4614a37a241152cce9f62bd0a600a00e59e6
Florian Schick <florian.schick@schick-software.de>
2019-12-28 15:10:09 +0100

    Logging switched to NLog.

--- FS.LaterList/Program.cs
+++ FS.LaterList/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System;
 
 namespace FS.LaterList
 {
@@ -7,14 +10,33 @@ namespace FS.LaterList
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logFactory = NLogBuilder.ConfigureNLog("FS.LaterList.nlog.config");
+            var logger = logFactory.GetCurrentClassLogger();
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(ex, "Execution of program stopped.");
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+            => Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureLogging(loggingBuilder =>
+                {
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
